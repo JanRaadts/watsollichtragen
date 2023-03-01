d09fa3ab91ac31c801fba909f9e8ef3aa3753f19
Jan Raadts <janraadts@outlook.de>
2023-03-01 20:10:38 +0100

    added some style and backup if location is not available

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { LottieModule } from 'ngx-lottie';
+import player from 'lottie-web';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { WeatherComponent } from './components/weather/weather.component';
+
+export function playerFactory(): any {  
+  return import('lottie-web');
+}
 
 @NgModule({
   declarations: [
@@ -12,7 +21,11 @@ import { WeatherComponent } from './components/weather/weather.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    CommonModule,
+    // Add the module like so:    
+    LottieModule.forRoot({ player: playerFactory }),
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/weather/weather.component.ts
+++ src/app/components/weather/weather.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AnimationItem } from 'lottie-web';
+import { AnimationOptions } from 'ngx-lottie';
 
 @Component({
   selector: 'app-weather',
@@ -9,11 +11,21 @@ export class WeatherComponent {
 
   showWeather: boolean = true
   loading: boolean = false
+  locationInput: boolean = false
   data:any
   currentTemperatur: any = 0
-  lat: number = 0
-  lon: number = 0
-  url!: string 
+  coordinates: any 
+  lat: any = 0
+  lon: any = 0
+  url!: string
+  city!: string
+  GEO_API: string = "e9e1604216e7465488692640e2190af5"
+  geoUrl!: string 
+
+  // This is the option that uses the package's AnimationOption interface  
+  options: AnimationOptions = {    
+    path: '../../assets/lottie/9793-thermometer.json'  
+  };  
 
 
   constructor(){}
@@ -27,14 +39,14 @@ export class WeatherComponent {
     const successCallback = (position:any) => {
       this.lat = position.coords.latitude;
       this.lon = position.coords.longitude
-      console.log(this.lat, this.lon)
-      console.log(this.url)
       this.url = `https://api.open-meteo.com/v1/forecast?latitude=${this.lat}&longitude=${this.lon}&hourly=temperature_2m,weathercode&current_weather=true&timezone=Europe%2FBerlin`
       this.fetchWeather();
     };
     
     const errorCallback = () => {
       alert("standort nicht gefunden")
+      this.loading = false
+      this.locationInput = true
     };
     
     navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
@@ -52,4 +64,39 @@ export class WeatherComponent {
     this.currentTemperatur = this.data.current_weather.temperature
     console.log(this.currentTemperatur)
   }
+
+
+    getGeo() {
+      this.geoUrl = `https://api.geoapify.com/v1/geocode/search?text=${this.city}&apiKey=e5002b4ebd9540a481613abeccde07c7`
+      fetch(this.geoUrl).then((response) => response.json())
+      .then((quotesData) => {this.coordinates = quotesData; this.getLatLon()});
+
+  
+  }
+
+  getLatLon(){
+
+    console.log(this.coordinates)
+
+    if (this.coordinates.features.length === 0) {
+      alert(`Keinen Ort mit dem Namen gefunden`);
+    } else {
+      this.lon = `${this.coordinates.features[0].geometry.coordinates[0]}`;
+    this.lat = `${this.coordinates.features[0].geometry.coordinates[1]}`;
+    this.url = `https://api.open-meteo.com/v1/forecast?latitude=${this.lat}&longitude=${this.lon}&hourly=temperature_2m,weathercode&current_weather=true&timezone=Europe%2FBerlin`
+    this.fetchWeather();
+    }
+  }
+
+ onSubmit(){
+  this.geoUrl = `https://api.geoapify.com/v1/geocode/search?text=${this.city}&apiKey=e5002b4ebd9540a481613abeccde07c7`
+  this.getGeo()
+  this.city = ""
+ }
+
+ // This is the component function that binds to the animationCreated event from the package  
+ onAnimate(animationItem: AnimationItem): void {    
+  console.log(animationItem);  
+}
+
 }
